12c6ec23ce3524f40e1209869e9bc7d243151a9f
rain-kun <rainkun3@protonmail.com>
2021-03-08 03:47:27 +0530

    full updated

---
+++ src/BaseBody.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import menu from './menu.svg';
+
+export default function Body() {
+    return (
+        <div className="base-body">
+            body
+            {/* <div className="main-menu"> */}
+                <img src={menu} />
+            {/* </div> */}
+        </div>
+    )
+}
---
+++ src/BaseNavbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className="base-nav">
+            <div className="navbar-2">
+                <div className="navbar">
+                    <ol className="navbar-nav">
+                        <li className="nav-item">Home</li>
+                        <li className="nav-item">Home</li>
+                        <li className="nav-item">Home</li>
+                        <li className="nav-item">Home</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    )
+}
